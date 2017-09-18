a3d961860e4c3a5a5f61cfa02804d097b737f4ac
Pat Newell <pnewell+git@renttherunway.com>
2017-09-18 10:30:25 -0400

    Update Subscription Form Icon

--- public/custom-code.php
+++ public/custom-code.php
@@ -8,7 +8,7 @@ function subscription_form( $attrs, $content = null ) {
 	$form_html = '<form id="sendgrid_mc_email_form__about" class="mc_email_form" action="#sendgrid_mc_email_subscribe" method="post">'.
 		'<input name="sendgrid_mc_user_type" type="hidden" value="%2$s">'.
 		'<div><label class="sendgrid_mc_label sendgrid_mc_label_email" for="sendgrid_mc_email">%1$s:</label></div>'.
-		'<div class="search-form"><input id="sendgrid_mc_email" class="search-field" name="sendgrid_mc_email" required="" type="text" value="" placeholder="phil@tecmo.com"><button type="submit" class="search-submit"><svg class="icon icon-search" aria-hidden="true" role="img"> <use href="#icon-search" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-envelope-o"></use> </svg><span class="screen-reader-text">Get Access</span></button></div>'.
+		'<div class="search-form"><input id="sendgrid_mc_email" class="search-field" name="sendgrid_mc_email" required="" type="text" value="" placeholder="phil@tecmo.com"><button type="submit" class="search-submit"><svg class="icon icon-envelope-o" aria-hidden="true" role="img"> <use href="#icon-search" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-envelope-o"></use> </svg><span class="screen-reader-text">Get Access</span></button></div>'.
 	'</form>';
 
 	return do_shortcode( sprintf(
